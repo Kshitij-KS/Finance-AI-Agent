c486ac9996a540051d7ddf3fc5cbea6ebcfab44d
Kshitij-KS <kshitijsingh2727@gmail.com>
2025-09-15 04:02:00 +0530

    Improved code readability along with  minor imporvements

--- Backend/FinanceAI.py
+++ Backend/FinanceAI.py
@@ -137,4 +137,5 @@ def handle_query():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # default to 5000 locally
     app.run(host="0.0.0.0", port=5000, debug=True)
